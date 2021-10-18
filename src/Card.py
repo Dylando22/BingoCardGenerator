be5edd08a938fd9fa9037b8ef6a19da2f95a9748
Erik on Voyager2 <ewfalor@gmail.com>
2021-10-18 11:25:52 -0600

    Bingo! deck generator starter code
    DuckieCorp's C++ team started this program but got stuck partway
    through.  As usual, they started writing code before making a plan and
    painted themselves into a corner.  They didn't even make a list of their
    classes, much less create a UML class diagram.
    
    In an effort to better understand the codebase I translated their work
    directly from C++ into Python.  We thought that it might be easier to
    finish this project by writing it with a more agile language than C++,
    but I am still stuck.
    
    This repo represents where the C++ guys ended up.  I think we should
    return to the drawing board and map out what we have before trying to
    add more code to this mess.
    
    I feel really bad about handing this over to the intern, but they've
    been studying UML in school.  Maybe they can turn this lemon in to
    lemonade...

---
+++ src/Card.py
@@ -0,0 +1,28 @@
+import sys
+
+import NumberSet
+
+
+class Card():
+    def __init__(self, idnum, size, numberSet):
+        """Card constructor"""
+        pass
+
+    def getId(self):
+        """Return an integer: the ID number of the card"""
+        pass
+
+    def getSize(self):
+        """Return an integer: the size of one dimension of the card.
+        A 3x3 card will return 3, a 5x5 card will return 5, etc.
+        """
+        pass
+
+    def getSquare(self, row, col):
+        """Return the value in the Bingo square at (row, col) """
+        pass
+
+    def print(self, file=sys.stdout):
+        """void function:
+        Prints a card to the screen or to an open file object"""
+        pass
---
+++ src/UserInterface.py
@@ -0,0 +1,81 @@
+import Deck
+import Menu
+
+
+class UserInterface():
+    def __init__(self):
+        pass
+
+    def run(self):
+        """Present the main menu to the user and repeatedly prompt for a valid command"""
+        print("Welcome to the Bingo! Deck Generator\n")
+        menu = Menu.Menu("Main")
+        menu.addOption("C", "Create a new deck")
+        
+        keepGoing = True
+        while keepGoing:
+            command = menu.show()
+            if command == "C":
+                pass
+            elif command == "X":
+                keepGoing = False
+
+    def __createDeck(self):
+        """Command to create a new Deck"""
+        # TODO: Get the user to specify the card size, max number, and number of cards
+
+        # TODO: Create a new deck
+
+        # TODO: Display a deck menu and allow use to do things with the deck
+        pass
+
+    def __getIntegerInput(self, m, n):
+        """
+        Prompt the user for an integer in the range [m, n] INCLUSIVE
+        If the provided input is NOT an integer NOR in the range, repeat the prompt
+        Otherwise, convert the user's value to an integer and return it.
+        """
+        pass
+
+    def __getStringInput(self):
+        """
+        Prompt the user for a string and return it
+        """
+        pass
+
+    def __deckMenu(self):
+        """Present the deck menu to user until a valid selection is chosen"""
+        menu = Menu.Menu("Deck")
+        menu.addOption("P", "Print a card to the screen")
+        menu.addOption("D", "Display the whole deck to the screen")
+        menu.addOption("S", "Save the whole deck to a file")
+
+        keepGoing = True
+        while keepGoing:
+            command = menu.show()
+            if command == "P":
+                self.__printCard()
+            elif command == "D":
+                print()
+                self.__m_currentDeck.print()
+            elif command == "S":
+                self.__saveDeck()
+            elif command == "X":
+                keepGoing = False
+
+    def __printCard(self):
+        """Command to print a single card"""
+        cardToPrint = self.__getNumberInput("Id of card to print", 1, self.__m_currentDeck.getCardCount())
+        if cardToPrint > 0:
+            print()
+            self.__m_currentDeck.print(idx=cardToPrint)
+
+    def __saveDeck(self):
+        """Command to save a deck to a file"""
+        fileName = self.__getStringInput("Enter output file name")
+        if fileName != "":
+            # TODO: open a file and pass to currentDeck.print()
+            outputStream = open(fileName, 'w')
+            self.__m_currentDeck.print(outputStream)
+            outputStream.close()
+            print("Done!")
